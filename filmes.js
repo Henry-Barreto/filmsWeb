24603c1d9035c23970359b8fdbb6189ae678a312
Henrique Barreto <Henrique040701@gmail.com>
2025-03-06 20:11:19 -0300

    Trazendo arquivos dos meu primeiro projeto
    ele esta aqui como parâmetro de comparação para meus futuros projetos , como uma forma de avaliar minha evolução

---
+++ filmes.js
@@ -0,0 +1,74 @@
+// const $menuaction = document.getElementById('menu1');
+// const $menutrigger = document.getElementById('menu-trigger1');
+// let state = $menuaction.dataset.open;
+// $menutrigger.addEventListener('click',() => {  // if(state=="false"){     // state="true";  // }else{     // state="false";  // }  // $menuaction.dataset.open = state; // } )
+
+function openActions() {
+  document.getElementById("actionsDiv").classList.toggle("show");
+}
+
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+
+function openRomance() {
+  document.getElementById("romanceDiv").classList.toggle("show");
+}
+
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+
+function openComedy() {
+  document.getElementById("comedyDiv").classList.toggle("show");
+}
+
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+
+function openTerror() {
+  document.getElementById("terrorDiv").classList.toggle("show");
+}
+
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+
+
